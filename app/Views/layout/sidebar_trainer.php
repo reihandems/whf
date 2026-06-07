c5198d876e62d27e08c54b8c5473fe45c2c0a66e
Reihan Demasiras Challada <reihandchallada@gmail.com>
2026-06-07 22:08:03 +0700

    Default

--- app/Views/layout/sidebar_trainer.php
+++ app/Views/layout/sidebar_trainer.php
@@ -47,14 +47,6 @@
                     Booking
                 </a>
             </li>
-            <li>
-                <a href="#" class="flex flex-row items-center gap-2 my-1 p-3 <?= ($menu == 'profil') ? 'bg-base-300 text-primary-500 font-bold' : 'text-gray-400' ?>" </a>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
-                        <path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clip-rule="evenodd" />
-                    </svg>
-                    Profil
-                </a>
-            </li>
             <li class="mt-auto">
                 <a href="<?= base_url('/logout') ?>" class="flex flex-row items-center gap-2 my-1 p-3 btn btn-error btn-soft">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">

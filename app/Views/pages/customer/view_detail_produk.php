76e4ad2250c1ff2f2b898798a515aa63d914588e
Reihan Demasiras Challada <reihandchallada@gmail.com>
2026-06-08 22:15:15 +0700

    fixed: post login product and detail product page

--- app/Views/pages/customer/view_detail_produk.php
+++ app/Views/pages/customer/view_detail_produk.php
@@ -2,7 +2,7 @@
 <?= $this->section('content') ?>
 <!-- Content -->
 <div class="col-span-12 px-4 md:px-12 py-8">
-    <a href="<?= base_url('/home') ?>" class="btn mb-5">
+    <a href="<?= base_url('/produk') ?>" class="btn mb-5">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
             <path fill-rule="evenodd" d="M11.03 3.97a.75.75 0 0 1 0 1.06l-6.22 6.22H21a.75.75 0 0 1 0 1.5H4.81l6.22 6.22a.75.75 0 1 1-1.06 1.06l-7.5-7.5a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 0 1 1.06 0Z" clip-rule="evenodd" />
         </svg>
